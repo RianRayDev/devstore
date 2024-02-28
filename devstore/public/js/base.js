5318c1a0e48c583716c5fec41e1a74be2b2b526c
Rian Ray <rianraydeasis@gmail.com>
2024-02-28 17:19:35 +0800

    huge changes to header

---
+++ devstore/public/js/base.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const menuButton = document.querySelector('.navbar-toggler');
+    const cartIcon = document.querySelector('.cart');
+    const focusRing = document.querySelector('.focus-ring');
+    const divHeader = document.getElementById('div-header');
+
+    cartIcon.style.transition = "opacity 1s ease";
+    menuButton.style.transition = "opacity 1s ease";
+
+    function toggleIcons() {
+        const isMobile = window.innerWidth <= 990; // Define the width at which the switch happens (adjust as needed)
+        if (isMobile) {
+            // Mobile view: show menu button, hide cart icon
+            menuButton.style.opacity = 1;
+            cartIcon.style.opacity = 0;
+        } else {
+            // Desktop view: hide menu button, show cart icon
+            menuButton.style.opacity = 0;
+            cartIcon.style.opacity = 1;
+        }
+    }
+
+    function containerHeader() {
+        const isSm = window.innerWidth <= 576; // Define the width at which the switch happens (adjust as needed)
+        if (isSm) {
+            divHeader.classList.add('container');
+        } else {
+            divHeader.classList.remove('container');
+        }
+    }
+    // Initial call to toggleIcons
+    toggleIcons();
+    containerHeader();
+    // Listen for window resize events
+    window.addEventListener('resize', () => {
+        toggleIcons();
+        containerHeader();
+    });
+});
